d4825c5feb86e7ba8ce19d55cad466c4293033d6
Raghav14200 <raghav142001@gmail.com>
2021-03-22 21:07:33 +0530

    giphy

---
+++ one_month/GiphySearch/GiphySearch/javascript/main.js
@@ -0,0 +1,8 @@
+//alert("hi world");
+let input=document.querySelector(".js-userinput");
+let button=document.querySelector(".js-go");
+//console.log(input);
+
+button.addEventListener("click",()=>{
+   console.log(input.value);
+})
