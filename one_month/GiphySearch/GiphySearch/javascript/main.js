4b3ddafea291a7fa23aa312b0da6e344525a7474
Raghav14200 <raghav142001@gmail.com>
2021-03-23 18:49:42 +0530

    giphy

--- one_month/GiphySearch/GiphySearch/javascript/main.js
+++ one_month/GiphySearch/GiphySearch/javascript/main.js
@@ -1,8 +1,37 @@
 //alert("hi world");
 let input=document.querySelector(".js-userinput");
 let button=document.querySelector(".js-go");
-//console.log(input);
 
 button.addEventListener("click",()=>{
-   console.log(input.value);
+   makerequest(input);
 })
+
+
+var obj;
+
+// AJAX Request
+function makerequest(input){
+var GiphyAJAXCall = new XMLHttpRequest();
+var url = "http://api.giphy.com/v1/gifs/search?q="+input.value+"&api_key=dc6zaTOxFJmzC";
+GiphyAJAXCall.open( 'GET', url );
+GiphyAJAXCall.send();
+GiphyAJAXCall.addEventListener("load",(e)=>{
+   push_to_dom(e.target.response);
+})
+}
+
+input.addEventListener("keyup",(e)=>{
+   if(e.key=="Enter"){
+      makerequest(input);
+   }
+})
+
+function push_to_dom(data){
+   var resp=JSON.parse(data);
+      console.log(resp.data[0].images.fixed_height.url);
+      document.querySelector(".js-container").innerHTML="";
+      resp.data.forEach(element => {
+         document.querySelector(".js-container").innerHTML+=`<img src="${element.images.fixed_height.url}" class="container-image"/>`;
+      });
+   
+}
