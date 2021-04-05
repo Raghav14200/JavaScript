a06184c05cb67d276f18c80c087c2c4ca71b99f0
Raghav14200 <raghav142001@gmail.com>
2021-04-05 18:48:42 +0530

    embed

--- one_month/SoundCloudPlayer/SoundCloudPlayer/javascript/main.js
+++ one_month/SoundCloudPlayer/SoundCloudPlayer/javascript/main.js
@@ -11,17 +11,68 @@ SoundCloudAPI.getTrack=function(inputValue){
 SC.get('/tracks', {
   q: inputValue
 }).then(function(tracks) {
-  console.log(tracks);
+  SoundCloudAPI.renderTrack(tracks);
 });
 }
 
 SoundCloudAPI.init();
 SoundCloudAPI.getTrack("Rayle");
 
-SoundCloudAPI.renderTracks= function(){
-  var new_div=document.createElement('div');
-  new_div.classList.add("card");
-  document.querySelector(".js-search-results").appendChild(new_div);
+SoundCloudAPI.renderTrack = function( tracks) {
+
+      tracks.forEach(track=> {
+        console.log(track);
+        var card = document.createElement('div');
+			card.classList.add('card');
+
+			// image
+			var imageDiv = document.createElement('div');
+			imageDiv.classList.add('image');
+
+			var image_img = document.createElement('img');
+			image_img.classList.add('image_img');
+			image_img.src = track.artwork_url || 'http://lorempixel.com/100/100/abstract/';
+
+			imageDiv.appendChild( image_img );
+
+			// content
+			var content = document.createElement('div');
+			content.classList.add('content');
+
+			var header = document.createElement('div');
+			header.classList.add('header');
+			header.innerHTML = '<a href="'+track.permalink_url+'" target="_blank">'+track.title+'</a>';
+
+			// button
+			var button = document.createElement('div');
+			button.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
+
+			var icon = document.createElement('i');
+			icon.classList.add('add', 'icon');
+
+			var buttonText = document.createElement('span');
+			buttonText.innerHTML = 'Add to playlist';
+
+      content.appendChild(header);
+			button.appendChild( icon );
+			button.appendChild( buttonText );
+
+			// card
+			card.appendChild( imageDiv );
+			card.appendChild( content );
+			card.appendChild( button );
+
+      var searchResult=document.querySelector('.js-search-results');
+			searchResult.appendChild( card );
+      });
+			
 }
 
-SoundCloudAPI.renderTracks();
+SC.oEmbed('https://soundcloud.com/forss/flickermood', {
+  auto_play: true
+}).then(function(embed){
+  console.log('oEmbed response: ', embed);
+
+  var sidebar=document.querySelector('.js-playlist');
+  sidebar.innerHTML=embed.html;
+});
