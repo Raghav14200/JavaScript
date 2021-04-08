41b14104d5220154e949556e4eec01c8b141a004
Raghav14200 <raghav142001@gmail.com>
2021-04-08 12:37:21 +0530

    for_each_and_map

---
+++ one_month/array.js
@@ -0,0 +1,16 @@
+
+var a=[1,2,3];
+a.push(4,5);
+
+a.forEach((e)=>{
+console.log(e);
+});
+
+console.log(a.pop());
+console.log(a);
+
+a.shift();
+console.log(a);
+
+a.reverse();
+console.log(a);
---
+++ one_month/map.js
@@ -0,0 +1,4 @@
+var numbers=[1,2,4,6,8,123,245];
+var doubles=numbers.map((num)=>{
+	return num*2;
+});
